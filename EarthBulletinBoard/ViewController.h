b32f013a3931df68082f858de6dd4ebf662af9d0
Earth Ponce Maniebo <earth@codesignate.com>
2014-12-09 16:42:06 +0800

    Initial Commit

---
+++ EarthBulletinBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EarthBulletinBoard
+//
+//  Created by Earth Ponce Maniebo on 12/9/14.
+//  Copyright (c) 2014 Seer Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EarthBulletinBoard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EarthBulletinBoard
+//
+//  Created by Earth Ponce Maniebo on 12/9/14.
+//  Copyright (c) 2014 Seer Technologies, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
